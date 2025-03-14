896742a54bb81ec7b3a19cbb535fbd34a92ffd67
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-03-14 19:50:40 +0000

    Seeded groups table with test data

--- database/migrations/2025_03_14_192052_create_groups_table.php
+++ database/migrations/2025_03_14_192052_create_groups_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name', 100);
             $table->string('address');
             $table->string('city');
-            $table->string('postcode', 8);
+            $table->string('postcode');
             $table->integer('activity1');
             $table->integer('activity2')->nullable();
             $table->integer('activity3')->nullable();
---
+++ database/seeders/GroupSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 100; $i++){
+            DB::table('groups')->insert([
+                'name' => fake()->words(2, true),
+                'address' => fake()->streetAddress(),
+                'city' => fake()->city(),
+                'postcode' => fake()->postcode(),
+                'activity1' => fake()->numberBetween(1, 9),
+                'activity2' => fake()->numberBetween(1, 9),
+                'activity3' => fake()->numberBetween(1, 9),
+                'description' => fake()->paragraphs(3, true),
+                'image' => fake()->imageUrl(),
+                'contact_name' => fake()->name(),
+                'contact_email' => fake()->email()
+            ]);
+        }
+    }
+}
