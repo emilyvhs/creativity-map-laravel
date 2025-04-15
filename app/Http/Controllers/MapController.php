6230b8052bb0b7ed60f291054fb7588315c61b67
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-04-15 20:06:50 +0100

    Added if statement to check for results from geocoder before updating database

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class MapController extends Controller
 {
     //check if data exists in database
-    ////get the data from a query
+    //get the data from a query
     //check if the data gets results from geocoder
     //store the lat and long in the database
 
@@ -22,17 +22,14 @@ class MapController extends Controller
 
         foreach ($rows as $row) {
             $result = app('geocoder')->geocode($row->address)->get();
-            $coordinates = $result[0]->getCoordinates();
-            $lat = $coordinates->getLatitude();
-            $lng = $coordinates->getLongitude();
-            DB::table('groups')
-                ->where('id', $row->id)
-                ->update(['lat' => $lat, 'lng' => $lng]);
+            if($result) {
+                $coordinates = $result[0]->getCoordinates();
+                $lat = $coordinates->getLatitude();
+                $lng = $coordinates->getLongitude();
+                DB::table('groups')
+                    ->where('id', $row->id)
+                    ->update(['lat' => $lat, 'lng' => $lng]);
+            }
         }
-
-
-
-
-
     }
 }
