93dac5ba8529224814ea7ec3600b26f0dfe1d9c7
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-04-15 20:03:19 +0100

    Created working function to get lat/long coordinates from a street address

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MapController extends Controller
+{
+    //check if data exists in database
+    ////get the data from a query
+    //check if the data gets results from geocoder
+    //store the lat and long in the database
+
+    public function getCoordinates():void
+    {
+        $rows = DB::table('groups')
+            ->whereAny([
+                'lat',
+                'lng'], '=', null)
+            ->get();
+
+        foreach ($rows as $row) {
+            $result = app('geocoder')->geocode($row->address)->get();
+            $coordinates = $result[0]->getCoordinates();
+            $lat = $coordinates->getLatitude();
+            $lng = $coordinates->getLongitude();
+            DB::table('groups')
+                ->where('id', $row->id)
+                ->update(['lat' => $lat, 'lng' => $lng]);
+        }
+
+
+
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\GroupController;
+use App\Http\Controllers\MapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GroupController::class, 'search']);
 Route::get('groups', [GroupController::class, 'all']);
 Route::get('groups/{id}', [GroupController::class, 'find']);
+
+Route::get('map', [MapController::class, 'getCoordinates']);
