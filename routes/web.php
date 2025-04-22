907b1bdf7dbdcdcb1b7b9d5a8964f0ff795af0b9
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-04-22 06:39:14 +0100

    Moved addGroup/create functions to PendingGroupController

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,15 @@
 
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\MapController;
+use App\Http\Controllers\PendingGroupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GroupController::class, 'search']);
 Route::get('groups', [GroupController::class, 'all']);
-Route::get('groups/add', [GroupController::class, 'addGroup']);
 
-Route::post('groups/add', [GroupController::class, 'create']);
+Route::get('groups/add', [PendingGroupController::class, 'addGroup']);
+Route::post('groups/add', [PendingGroupController::class, 'create']);
 
 Route::get('groups/{id}', [GroupController::class, 'find']);
-
-
 
 Route::get('map', [MapController::class, 'getCoordinates']);
