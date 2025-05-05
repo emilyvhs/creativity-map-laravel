16a8c6e40953199eee94e7f6269ff45507cd14b9
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-05-05 08:27:59 +0100

    Commenting js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,21 +4,25 @@ let blurredSection = document.querySelector('.blurred')
 let approveModal = document.querySelector('.approve-modal')
 let deleteModal = document.querySelector('.delete-modal')
 
+//Displays a pop-up asking you to confirm you want to approve the group
 window.handleApproveClick = function() {
     blurredSection.classList.add('blur-sm')
     approveModal.classList.remove('hidden')
 }
 
+//Displays a pop-up asking you to confirm you want to delete the group
 window.handleDeleteClick = function() {
     blurredSection.classList.add('blur-sm')
     deleteModal.classList.remove('hidden')
 }
 
+//Closes the approval pop-up without action
 window.handleApproveModalCancel = function() {
     blurredSection.classList.remove('blur-sm')
     approveModal.classList.add('hidden')
 }
 
+//Closes the cancel pop-up without action
 window.handleDeleteModalCancel = function() {
     blurredSection.classList.remove('blur-sm')
     deleteModal.classList.add('hidden')
